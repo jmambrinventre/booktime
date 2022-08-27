756aed06e1ed156c50ab163030f199add8712b34
jmambrinventre <64538653+jmambrinventre@users.noreply.github.com>
2022-08-27 12:30:59 -0300

    about us page

--- booktime/urls.py
+++ booktime/urls.py
@@ -13,12 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',
-    TemplateView.as_view(template_name="home.html")),
+    path('', include('main.urls')),
+    # path('',
+    # TemplateView.as_view(template_name="base.html")),
 ]
+
+# from django.contrib import admin
+# from django.urls import path
+# from django.views.generic import TemplateView
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('',
+#     TemplateView.as_view(template_name="base.html")),
+# ]
