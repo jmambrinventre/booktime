4499d7f0b7fbb7251c117003fc25536e5055661e
jmambrinventre <64538653+jmambrinventre@users.noreply.github.com>
2022-08-27 13:55:07 -0300

    test contact us form

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,29 @@
+# from django.urls import path
+# from django.views.generic import TemplateView
+# urlpatterns = [
+#     path(
+#         "about-us/",
+#         TemplateView.as_view(template_name="about_us.html")),
+#     path(
+#         "",
+#         TemplateView.as_view(template_name="home.html")),
+#     path(
+#         "",
+#         TemplateView.as_view(template_name="base.html")),
+
+# ]
+
 from django.urls import path
 from django.views.generic import TemplateView
 urlpatterns = [
     path(
         "about-us/",
-        TemplateView.as_view(template_name="about_us.html")),
+        TemplateView.as_view(template_name="about_us.html"),
+        name="about_us",
+    ),
     path(
         "",
-        TemplateView.as_view(template_name="home.html")),
-    # path(
-    #     "",
-    #     TemplateView.as_view(template_name="base.html")),
-
+        TemplateView.as_view(template_name="home.html"),
+        name="home",
+    ),
 ]
